5508310f822d244c7c5b3514fed114410b5c07fb
Ali Al-Qallaf <ali@al-qallaf.net>
2019-06-13 17:11:36 +0100

    change path to dataset + generate .yaml instead of .txt

--- datasetscripts/GenerateFiles_BaxterImgNamesLinksEnvProInfo.py
+++ datasetscripts/GenerateFiles_BaxterImgNamesLinksEnvProInfo.py
@@ -1,16 +1,18 @@
-#level1-Case4
+#level1-Case3
+#swap information and generate files.
 
 import yaml
 import os
 import torch
 
 filespath = os.path.abspath(__package__)
-# Get information from baxter validation pro files
-propath = filespath + '/' + '20190521' + '/' + 'train' + '/' + 'env800try' + '/' + 'pro' + '/'
+filespath = os.path.dirname(filespath)
+# Get proprioception information
+propath = filespath + '/' + 'traindatacase3' + '/' + 'env' + '/' + 'pro' + '/'
 envpro = os.listdir(propath)
 
 #To store/match files names with same names in this folder.
-baxterImgPath = filespath + '/' + '20190521' + '/' + 'train' + '/' + 'baxter800try' + '/' + 'images' + '/'
+baxterImgPath = filespath + '/' + 'traindatacase3' + '/' + 'baxter' + '/' + 'images' + '/'
 baxterImg = os.listdir(baxterImgPath) #envpropath)
 
 
@@ -22,23 +24,23 @@ procount = 0
 yaml_files_changed_count = 0
 
 #allpropath = filespath + '/' + '20190429' + '/' + 'randomuncyned' + '/'
-newfilepropath = filespath + '/' + '20190521' + '/' + 'train' + '/' + 'baxter800try' + '/' + 'converted' + '/'
+newfilepropath = filespath + '/' + 'traindatacase3' + '/' + 'converted' + '/'
 
 
 def getprofilename(baxterimagefile):
     imagefilename = baxterimagefile.replace("image", "pro")
-    imagefilename = imagefilename.replace("jpg", "txt")
+    imagefilename = imagefilename.replace("jpg", "yaml")
     return imagefilename
 
 def get_yaml_information(envprofile):
     with open(propath + envprofile) as f:
-        doc = yaml.safe_load(f)
+        doc = yaml.load(f)
     return doc
 
 def store_env_info_into_filename_comply_with_baxter_image_filename(env_pro_dictionary, pro_filename_generated_to_comply_with_baxter_image_name):
     #totalprofiles[envprofile] = baxter_dictionary
     with open(newfilepropath + pro_filename_generated_to_comply_with_baxter_image_name, 'w') as f:
-        yaml.safe_dump(env_pro_dictionary, f)
+        yaml.dump(env_pro_dictionary, f)
 
 if __name__ == "__main__":
     #for file in sorted(envpro):
