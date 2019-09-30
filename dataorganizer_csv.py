98d5b906c8e9ef81c360f1a3d7c59b5b88c2181d
Ali Al-Qallaf <ali@al-qallaf.net>
2019-09-30 18:33:42 +0100

    handle one class

--- dataorganizer_csv.py
+++ dataorganizer_csv.py
@@ -7,6 +7,7 @@ import os
 # ******************
 DATASET = "20190925"
 # ******************
+theclass= "baxter"
 # ******************
 
 # ******************
@@ -24,7 +25,7 @@ with open(DATASET + ".csv", mode='w', newline='', encoding='utf-8') as dataset_f
     dataset_writer.writerow(['right', 'left', 'disparity', 'proprioception'])
 
     base_names = []
-    directory = os.fsencode(data_path +"/"+ DATASET + "/images_right")
+    directory = os.fsencode(data_path +"/"+ DATASET + "/" + theclass + "/images_right")
     for file in os.listdir(directory):
         filename = os.fsdecode(file)
         if filename.endswith(".jpg"):
@@ -36,15 +37,15 @@ with open(DATASET + ".csv", mode='w', newline='', encoding='utf-8') as dataset_f
 
     base_names.sort()
     for name in base_names:
-        img_right_path = data_path +"/"+ DATASET + "/images_right/" + name + ".jpg"
+        img_right_path = data_path +"/"+ DATASET + "/" + theclass + "/images_right/" + name + ".jpg"
 
-        img_left_path = data_path +"/"+ DATASET + "/images_left/" + name + ".jpg"
+        img_left_path = data_path +"/"+ DATASET + "/" + theclass + "/images_left/" + name + ".jpg"
         img_left_path = img_left_path.replace("imageright","imageleft")
 
-        img_disparity = data_path +"/"+ DATASET + "/images_disparity/" + name + ".yaml"
+        img_disparity = data_path +"/"+ DATASET + "/" + theclass + "/images_disparity/" + name + ".yaml"
         img_disparity = img_disparity.replace("imageright","disparity")
 
-        pro_path = data_path +"/"+ DATASET + "/pro/" + name + ".yaml"
+        pro_path = data_path +"/"+ DATASET + "/" + theclass + "/pro/" + name + ".yaml"
         pro_path = pro_path.replace("imageright","pro")
         
         dataset_writer.writerow([img_right_path, img_left_path, img_disparity, pro_path])
