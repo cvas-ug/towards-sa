8c025b8c2c78b4f475c5c2586a299e2165010cc0
Ali Al-Qallaf <ali@al-qallaf.net>
2019-10-01 14:54:33 +0100

    let script handle different dataset folders and export them into two classes of csv files

--- dataorganizer_csv.py
+++ dataorganizer_csv.py
@@ -6,6 +6,8 @@ import os
 # This script should be placed at the same level as the "data" directory
 # ******************
 DATASET = "20190925"
+folders_class = ["me", "env"]
+dataset_folders = ["fc", "fg"]
 # ******************
 theclass= "baxter"
 # ******************
@@ -17,37 +19,38 @@ currentWorkingFilePath = os.path.dirname(__file__)
 directoryPath = os.path.dirname(currentWorkingFilePath)
 
 data_path = directoryPath
+directories = os.listdir(directoryPath + "/" + DATASET)
 
-file_path = data_path +"/"+ DATASET + "/" + DATASET + ".csv"
-with open(DATASET + ".csv", mode='w', newline='', encoding='utf-8') as dataset_file:
-    dataset_writer = csv.writer(dataset_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+for aclass in folders_class:
+    file_path = data_path +"/"+ DATASET + "/" + DATASET + "_" + aclass + ".csv"
+    with open(DATASET + "_" + aclass + ".csv", mode='w', newline='', encoding='utf-8') as dataset_file:
+        dataset_writer = csv.writer(dataset_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        dataset_writer.writerow(['right', 'left', 'disparity', 'proprioception'])
+        for folder in dataset_folders:
+                base_names = []
+                directory = os.fsencode(data_path +"/"+ DATASET + "/images_right_"+folder+aclass)
+                for file in os.listdir(directory):
+                    filename = os.fsdecode(file)
+                    if filename.endswith(".jpg"):
+                        #print(filename[:-4])
+                        base_names.append(filename[:-4])
+                        continue
+                    else:
+                        continue
 
-    dataset_writer.writerow(['right', 'left', 'disparity', 'proprioception'])
+                base_names.sort()
+                for name in base_names:
+                    img_right_path = data_path +"/"+ DATASET + "/images_right_"+folder+aclass+"/"+ name + ".jpg"
 
-    base_names = []
-    directory = os.fsencode(data_path +"/"+ DATASET + "/" + theclass + "/images_right")
-    for file in os.listdir(directory):
-        filename = os.fsdecode(file)
-        if filename.endswith(".jpg"):
-            #print(filename[:-4])
-            base_names.append(filename[:-4])
-            continue
-        else:
-            continue
+                    img_left_path = data_path +"/"+ DATASET + "/images_left_"+folder+aclass+"/"+ name + ".jpg"
+                    img_left_path = img_left_path.replace("imageright","imageleft")
 
-    base_names.sort()
-    for name in base_names:
-        img_right_path = data_path +"/"+ DATASET + "/" + theclass + "/images_right/" + name + ".jpg"
+                    img_disparity = data_path +"/"+ DATASET + "/images_disparity_"+folder+aclass+"/"+ name +".yaml"
+                    img_disparity = img_disparity.replace("imageright","disparity")
 
-        img_left_path = data_path +"/"+ DATASET + "/" + theclass + "/images_left/" + name + ".jpg"
-        img_left_path = img_left_path.replace("imageright","imageleft")
-
-        img_disparity = data_path +"/"+ DATASET + "/" + theclass + "/images_disparity/" + name + ".yaml"
-        img_disparity = img_disparity.replace("imageright","disparity")
-
-        pro_path = data_path +"/"+ DATASET + "/" + theclass + "/pro/" + name + ".yaml"
-        pro_path = pro_path.replace("imageright","pro")
-        
-        dataset_writer.writerow([img_right_path, img_left_path, img_disparity, pro_path])
+                    pro_path = data_path +"/"+ DATASET + "/" + "pro_"+folder+aclass+"/"+ name + ".yaml"
+                    pro_path = pro_path.replace("imageright","pro")
+                    
+                    dataset_writer.writerow([img_right_path, img_left_path, img_disparity, pro_path])
 
 print("Dataset mapping to csv, Done!")
